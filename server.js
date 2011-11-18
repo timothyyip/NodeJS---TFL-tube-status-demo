a9c88576dcf9b43b914bbc61e2a606238a2e6dab
Timothy Yip <tim@timothyyip.org>
2011-11-18 16:22:30 +0000

    northbound and southbound now fixed, also uses bootstrap twispy to display additional information

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var sys = require('util'), http = require('http'), datejs = require('datejs');
 var XMLHttpRequest = require("XMLHttpRequest").XMLHttpRequest, xhr = new XMLHttpRequest();
 var xml2js = require('xml2js'), parser = new xml2js.Parser();
-var north= new Array( ), south =  new Array( );
+var trains = new Array();
 
 var getMMSSInSeconds = function(timeString) {
 	var time = Date.parse('January 1, 1970 00:' + timeString);
@@ -14,88 +14,85 @@ xhr.onreadystatechange = function() {
 		//	parser.write(this.responseText).end();
 
 		parser.addListener('end', function(result) {
+			trains= new Array();
+			
 			for(var sCounter = 0; sCounter < result.S.length; sCounter++) {
+
 				for(var pCounter = 0; pCounter < result.S[sCounter].P.length; pCounter++) {
 
 					var isNorth = (result.S[sCounter].P[pCounter]['@'].N.indexOf("Northbound") != -1);
+
 					if(result.S[sCounter].P[pCounter].T) {
+
 						for(var tCounter = 0; tCounter < result.S[sCounter].P[pCounter].T.length; tCounter++) {
+
+							var trainId = result.S[sCounter].P[pCounter].T[tCounter]['@'].S;
+
 							var trainLocation = result.S[sCounter].P[pCounter].T[tCounter]['@'].L;
-
-							if(isNorth) {
-
-								if(north[trainLocation]) {
-									if(getMMSSInSeconds(result.S[sCounter].P[pCounter].T[tCounter]['@'].C)) {//if time to station is valid
-										if(getMMSSInSeconds(result.S[sCounter].P[pCounter].T[tCounter]['@'].C) < north[trainLocation]) {//if this tts is less than stored tts
-											north[trainLocation] = getMMSSInSeconds(result.S[sCounter].P[pCounter].T[tCounter]['@'].C);
-											//store this tts
-										}
-									} else {
-										north[trainLocation] = 0;
-										//is tts is invalid then its already at a station effectively meaning tts is 0
-									}
-								} else {
-									north[trainLocation] = getMMSSInSeconds(result.S[sCounter].P[pCounter].T[tCounter]['@'].C);
-								}
-
-							} else {//TODO: I hate this, must be a way to use some kind of reflection for this
-								
-								if(south[trainLocation]) {
-								
-									if(getMMSSInSeconds(result.S[sCounter].P[pCounter].T[tCounter]['@'].C)) {//if time to station is valid
-										if(getMMSSInSeconds(result.S[sCounter].P[pCounter].T[tCounter]['@'].C) < south[trainLocation]) {//if this tts is less than stored tts
-											south[trainLocation] = getMMSSInSeconds(result.S[sCounter].P[pCounter].T[tCounter]['@'].C);
-											//store this tts
-										}
-									} else {
-										
-										south[trainLocation] = 0;
-										//is tts is invalid then its already at a station effectively meaning tts is 0
-									}
-								} else {
-									south[trainLocation] = getMMSSInSeconds(result.S[sCounter].P[pCounter].T[tCounter]['@'].C);
-								}
-
+							var trainIdIdentifier = trainLocation;
+							var trainTimeTillStation = getMMSSInSeconds(result.S[sCounter].P[pCounter].T[tCounter]['@'].C);
+							if(!trainTimeTillStation) {
+								trainTimeTillStation = 0;
 							}
+
+							if(trains[trainIdIdentifier]) {
+
+								if(trainTimeTillStation < trains[trainIdIdentifier].tts) {//if this tts is less than stored tts
+									trains[trainIdIdentifier].tts = trainTimeTillStation;
+									trains[trainIdIdentifier].isNorth = isNorth;
+								}
+
+							} else {
+
+								trains[trainIdIdentifier] = {
+									'id' : trainId,
+									'tts' : trainTimeTillStation,
+									'isNorth' : isNorth,
+									'location' : trainLocation
+								};
+							}
+
 						}
 					}
 				}
 			}
-			console.log(north);
-			console.log(south);
+			
+			var trainarray = [];
+			for(var prop in trains) {
+				trainarray.push(trains[prop])
+			}
+			
 		});
 		try {
 			parser.parseString(this.responseText.replace(/^\uFEFF/, ''));
-			
-			
+
 		} catch(err) {
 			throw err;
 		}
 	}
 
 };
-//setInterval(function() { north = [], south = [];
-xhr.open("GET", "http://cloud.tfl.gov.uk/TrackerNet/PredictionSummary/V");
-xhr.send();
-//}, 30000);
-/**
- var app = require('express').createServer(), io = require('socket.io').listen(app);
+setInterval(function() { north = [], south = [];
+	xhr.open("GET", "http://cloud.tfl.gov.uk/TrackerNet/PredictionSummary/V");
+	xhr.send();
+}, 30000);
+var app = require('express').createServer(), io = require('socket.io').listen(app);
 
- app.listen(4000);
+app.listen(4000);
 
- setInterval(function() {
- io.sockets.emit('message', {
- north : north,
- south : south,
- result : parseresult
- });
- }, 10000);
+setInterval(function() {
+	console.log(trains.toString());
+	var trainarray = [];
+	for(var prop in trains) {
+		trainarray.push(trains[prop])
+	}
+	io.sockets.emit('message', trainarray);
+}, 10000);
 
- app.get('/', function(req, res) {
- res.sendfile(__dirname + '/connect.html');
- });
+app.get('/', function(req, res) {
+	res.sendfile(__dirname + '/connect.html');
+});
 
- app.get('/mapping.js', function(req, res) {
- res.sendfile(__dirname + '/mapping.js');
- });
- **/
+app.get('/mapping.js', function(req, res) {
+	res.sendfile(__dirname + '/mapping.js');
+});
